1059df63d066d378fc4ec6b57120d6c90884e7ad
someoneemily <iemily.cwx@gmail.com>
2017-04-24 12:02:25 -0500

    added deleted package to Tester

--- src/com/google/codeu/codingchallenge/Tester.java
+++ src/com/google/codeu/codingchallenge/Tester.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
+package codinggoogle;
 
 import java.util.Map;
 import java.util.HashMap;
